eb199da1bea2a827c61d3114a2012633e9c21865
deerschen <deerschen@163.com>
2018-08-03 12:33:16 +0800

    test

--- src/components/pages/Test.vue
+++ src/components/pages/Test.vue
@@ -1,21 +1,134 @@
 <template>
-  <div class="template">
-    <p>{</p>
-      <template v-for="(value, key) in config">
-        <div :key="key" v-if="value.type === 'str'" >
-          <span>{{key}}:</span> <span>{{ value.value }}</span>
-        </div>
-        <template v-esle v-for="(subValue, subKey) in value.value">
-          <div :key="subKey" v-if="subValue.type === 'str'" >
-           <span>{{subKey}}:</span> <span>{{ subValue.value }}</span>
-          </div>
-        </template>
-      </template>
-    <p>}</p>
+  <div>
+    <div class="template">
+      <temp></temp>
+    </div>
+    <button @click="click">点击</button>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+const json = {
+  sex: 'male',
+  name: {
+    name: 'zhangsan',
+    age: [
+      {
+        age: '333'
+      }
+    ]
+  },
+  age: [
+    '333', '4444', '5555', '6666', {second: '2222'}, [
+      '777', '888', '999'
+    ]
+  ],
+  class: {
+    new: '222',
+    old: '333',
+    whether: '444'
+  }
+}
+
+Vue.component('temp', {
+  render: function (h) {
+    const render = format(h, json)
+    return h('div', [
+      h('div', render)
+    ])
+  },
+  props: {
+    show: {
+      type: Boolean,
+      default: false
+    }
+  }
+})
+
+function format (h, json) {
+  const arr = []
+  if (Array.isArray(json)) {
+    for (let i = 0; i < json.length; i++) {
+      if (typeof json[i] === 'string') {
+        arr.push(h('div', {
+          class: '222'
+        }, [
+          h('input', {
+            domProps: {
+              value: json[i]
+            },
+            on: {
+              input: function (event) {
+                json[i] = event.target.value
+              }
+            }
+          }),
+          h('span', ',')
+        ]))
+      } else if (Array.isArray(json[i])) {
+        const render = format(h, json[i])
+        arr.push(h('div', [
+          h('p', '['),
+          h('div', {
+            class: 'retract'
+          }, render),
+          h('p', ']' + ',')
+        ]))
+      } else {
+        const render = format(h, json[i])
+        arr.push(h('div', [
+          h('p', '{'),
+          h('div', {
+            class: 'retract'
+          }, render),
+          h('p', '}' + ',')
+        ]))
+      }
+    }
+  } else {
+    for (let key in json) {
+      if (typeof json[key] === 'string') {
+        arr.push(h('div', {
+          class: '222'
+        }, [
+          h('span', key + ': '),
+          h('input', {
+            domProps: {
+              value: json[key]
+            },
+            on: {
+              input: function (event) {
+                json[key] = event.target.value
+              }
+            }
+          }),
+          h('span', ',')
+        ]))
+      } else if (Array.isArray(json[key])) {
+        const render = format(h, json[key])
+        arr.push(h('div', [
+          h('p', key + ': ' + '['),
+          h('div', {
+            class: 'retract'
+          }, render),
+          h('p', ']' + ',')
+        ]))
+      } else {
+        const render = format(h, json[key])
+        arr.push(h('div', [
+          h('p', key + ': ' + '{'),
+          h('div', {
+            class: 'retract'
+          }, render),
+          h('p', '}' + ',')
+        ]))
+      }
+    }
+  }
+  return arr
+}
+
 export default {
   data () {
     return {
@@ -29,31 +142,12 @@ export default {
             age: 333
           }
         ]
-      },
-      config: {
-        sex: {
-          value: 'male',
-          type: 'str'
-        },
-        name: {
-          type: 'obj',
-          value: {
-            name: {
-              value: 'zhangsan',
-              type: 'str'
-            }
-          }
-        },
-        age: {
-          type: 'arr',
-          value: {
-            age: {
-              type: 'str',
-              value: 33
-            }
-          }
-        }
       }
+    }
+  },
+  methods: {
+    click () {
+      console.log(json)
     }
   }
 }
@@ -61,10 +155,13 @@ export default {
 
 <style lang="scss" scoped>
   .template{
-    width: 500px;
-    height: 500px;
+    // width: 500px;
+    // height: 500px;
     margin: 100px;
     border: 1px solid #ccc;
     padding: 20px;
+  }
+  .retract {
+    margin-left: 10px;
   }
 </style>
