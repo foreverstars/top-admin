3c354eafb01285d481c8d72e0d91c19006180c4e
deerschen <deerschen@163.com>
2018-09-12 20:07:17 +0800

    upload

--- src/components/common/articleList.vue
+++ src/components/common/articleList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="article-list">
-    <div class="main-li" v-for="(item, index) in data" :key="index">
+    <div class="main-li" v-for="(item, index) in data" :key="index" @click="goRouter(item)">
       <Card>
         <h2>{{ item.title }}</h2>
         <div class="desc">
@@ -9,11 +9,11 @@
         </div>
         <div class="info">
           <span>
-            <i></i>
+            <Icon type="ios-pricetag" />
             <span>{{ item.type }}</span>
           </span>
           <span>
-            <i></i>
+            <Icon type="md-time" />
             <span>{{ item.time }}</span>
           </span>
         </div>
@@ -31,7 +31,8 @@ export default {
   },
   props: ['data'],
   methods: {
-    goRouter(id) {
+    goRouter(row) {
+      const id = row.id
       this.$router.push({ name: 'Detail', params: { id } })
     }
   }
@@ -43,16 +44,30 @@ export default {
   .main-li {
     padding: 10px 20px;
     margin: 10px 0;
+    cursor: pointer;
   }
   .desc {
     margin-top: 5px;
+    height: 100px;
     img {
-      display: inline-block;
+      display: block;
+      width: 150px;
+      height: 100px;
+      float:left;
     }
     p {
-      display: inline-block;
+      float: right;
+      width: 760px;
+      text-indent: 20px;
       margin-left: 10px;
       vertical-align: top;
+      text-overflow: ellipsis;
+      overflow: hidden;
+      display: -webkit-box;
+      -webkit-line-clamp: 3;
+      -webkit-box-orient: vertical;
+      font-size: 13px;
+      color: gray;
     }
   }
   .info {
